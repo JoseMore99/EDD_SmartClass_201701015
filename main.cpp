f8a76aa53fee2ebb284563faba00f43d80cd1d4e
PaulaGar <57239404+JoseMore99@users.noreply.github.com>
2021-08-03 21:08:58 -0600

    Menu inicial realizado

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int opcion;
+    while (opcion !=5)
+    {
+        cout<<"++++++++++MENU+++++++++++"<<endl;
+        cout<<"1. Carga de Usuarios"<<endl;
+        cout<<"2. Carga de Tareas"<<endl;
+        cout<<"3. Ingreso Manual"<<endl;
+        cout<<"4. Reportes"<<endl;
+        cout<<"5. Salir"<<endl;
+        cout<<"+++++++++++++++++++++++++"<<endl;
+        cout<<"ingrese una opcion: ";
+        cin>>opcion;
+        switch (opcion)
+        {
+        case 1:
+            cout<<"Usuarios"<<endl;
+            break;
+        case 2:
+            cout<<"tareas"<<endl;
+            break;
+        case 3:
+            cout<<"Manual"<<endl;
+            break;
+        case 4:
+            cout<<"Repportes"<<endl;
+            break;
+        case 5:
+            cout<<"FIN"<<endl;
+            break;
+        default:
+            cout<<"Seleccione una opcion correcta"<<endl;
+            break;
+        }
+    }
+
+    system("pause");
+    return 0;
+}
